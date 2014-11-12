88ad3949bf57442992e3ca8c40c2774e0cfc7512
Rubykuby <rubykuby@gmail.com>
2014-11-12 11:25:58 +0100

    Initial commit

---
+++ src/main/java/org/nhl/containing_backend/Main.java
@@ -0,0 +1,13 @@
+package org.nhl.containing_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
