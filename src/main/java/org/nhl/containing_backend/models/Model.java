8e956141bdf2ac5a02a965c6cdea437169a37844
Rubykuby <rubykuby@gmail.com>
2014-11-20 11:41:13 +0100

    Added Model. Needs a lot of work.

---
+++ src/main/java/org/nhl/containing_backend/models/Model.java
@@ -0,0 +1,15 @@
+package org.nhl.containing_backend.models;
+
+import org.nhl.containing_backend.cranes.MoveableCrane;
+import org.nhl.containing_backend.cranes.RailCrane;
+import org.nhl.containing_backend.vehicles.Agv;
+import org.nhl.containing_backend.vehicles.Transporter;
+
+import java.util.List;
+
+/**
+ * Holds the data of the controller.
+ */
+public class Model {
+    private List<Agv> agvs;
+}
