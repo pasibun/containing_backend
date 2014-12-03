47d6e24031bd7a2869b8dd4dc99ce2a094ce6a60
Rubykuby <rubykuby@gmail.com>
2014-12-03 09:49:58 +0100

    Removed faulty test code. TODO

---
+++ src/test/java/org/nhl/containing_backend/TestDijkstra.java
@@ -0,0 +1,40 @@
+package org.nhl.containing_backend;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Matthijs
+ */
+public class TestDijkstra {
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testComputePaths() {
+        // TODO
+    }
+
+    @Test
+    public void testGetShortestPathTo() {
+        // TODO
+    }
+}
