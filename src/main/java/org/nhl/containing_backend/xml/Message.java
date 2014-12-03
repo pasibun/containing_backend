9057a8b1993616368e108235fe9cd000bc02ba18
Rubykuby <rubykuby@gmail.com>
2014-12-03 10:17:01 +0100

    Reformatted getters and setters. Added a way to automatically increment and assign ID

--- src/main/java/org/nhl/containing_backend/xml/Message.java
+++ src/main/java/org/nhl/containing_backend/xml/Message.java
@@ -1,47 +1,47 @@
 package org.nhl.containing_backend.xml;
 
-enum Command {
-
-    OK
-}
-
 
 public class Message {
+    private static int counter = 0;
     private String objectName;
     private int  objectId;
     private int objectSize;
     private Command command;
 
-    public void setObjectId(int objectId) {
-        this.objectId = objectId;
-    }
-
-    public void setObjectName(String objectName) {
-        this.objectName = objectName;
-    }
-
-    public void setObjectSize(int objectSize) {
-        this.objectSize = objectSize;
-    }
-
-    public void setCommand(Command command) {
-        this.command = command;
-    }
-
-    public Command getCommand() {
-        return command;
-    }
-
-    public int getObjectId() {
-        return objectId;
+    public Message() {
+        counter += 1;
+        objectId = counter;
     }
 
     public String getObjectName() {
         return objectName;
     }
 
+    public void setObjectName(String objectName) {
+        this.objectName = objectName;
+    }
+
+    public int getObjectId() {
+        return objectId;
+    }
+
     public int getObjectSize() {
         return objectSize;
     }
-    
+
+    public void setObjectSize(int objectSize) {
+        this.objectSize = objectSize;
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public static enum Command {
+        OK
+    }
 }
