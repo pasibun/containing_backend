18454928e87bc3911c584a786d1c346f44f63fb9
pasibun <matthijs_hovestad@hotmail.com>
2014-12-16 15:19:18 +0100

    CREATE move en depart commands. edit crane command

--- src/main/java/org/nhl/containing_backend/communication/messages/Message.java
+++ src/main/java/org/nhl/containing_backend/communication/messages/Message.java
@@ -6,7 +6,9 @@ public abstract class Message {
     public static final int ARRIVE = 2;
     public static final int SPEED = 3;
     public static final int CRANE = 4;
-    
+    public static final int MOVE = 5;
+    public static final int DEPART = 6;
+
     private static int counter = 0;
     private final int messageType;
     private int id;
