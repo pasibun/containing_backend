c2c59bc6e0106e12b0da21c64d34e3a3967236bb
pasibun <matthijs_hovestad@hotmail.com>
2014-12-17 13:43:32 +0100

    sdf

--- src/main/java/org/nhl/containing_backend/communication/messages/MoveMessage.java
+++ src/main/java/org/nhl/containing_backend/communication/messages/MoveMessage.java
@@ -1,6 +1,8 @@
 package org.nhl.containing_backend.communication.messages;
 
+import es.usc.citius.hipster.util.graph.HipsterDirectedGraph;
 import org.nhl.containing_backend.Dijkstra;
+import org.nhl.containing_backend.cranes.Crane;
 import org.nhl.containing_backend.vehicles.Agv;
 import org.nhl.containing_backend.vehicles.Transporter;
 
@@ -10,38 +12,38 @@ import org.nhl.containing_backend.vehicles.Transporter;
 public class MoveMessage extends Message {
 
     private Agv agv;
-    private Dijkstra dijkstra;
-    private int depotIndex;
+    private HipsterDirectedGraph<String, Double> graph;
+    private Crane crane;
 
-    public MoveMessage(Agv agv, Dijkstra dijkstra, int depotIndex) {
+    public MoveMessage(Agv agv, HipsterDirectedGraph<String, Double> graph, Crane crane) {
         super(Message.MOVE);
         this.agv = agv;
-        this.dijkstra = dijkstra;
-        this.depotIndex = depotIndex;
+        this.graph = graph;
+        this.crane = crane;
     }
 
     public String generateXml() {
         String message = "";
         message += "<id>" + getId() + "</id>";
         message += "<Move>";
-        message += "<currentLocationX>" + agv.getX() + "</currentLocationX>";
-        message += "<currentLocationY>" + agv.getY() + "</currentLocationY>";
+        message += "<CurrentX>" + agv.getX() + "</CurrentX>";
+        message += "<CurrentY>" + agv.getY() + "</CurrentY>";
         message += "<Dijkstra>";
 
+        message += "</Dijkstra>";
+        message += "<EndLocationType>" + crane.getType() + "</EndLocationType>";
+        message += "<EndLocationId>" + crane.getId() + "</EndLocationId>";
         message += "</Move>";
 
         return message;
     }
 
+    private String toXml(HipsterDirectedGraph<String, Double> graph) {
+
+        return null;
+    }
+
     public Agv getAgv() {
         return agv;
-    }
-
-    public Dijkstra getDijkstra() {
-        return dijkstra;
-    }
-
-    public int getDepotIndex() {
-        return depotIndex;
     }
 }
